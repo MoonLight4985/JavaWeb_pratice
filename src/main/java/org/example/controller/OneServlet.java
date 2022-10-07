aeb6b2252f017c94600e7718ceccde114a0f1e69
moonlight <szy601870112@gmail.com>
2022-10-07 09:40:53 +0800

    session基本知识

---
+++ src/main/java/org/example/controller/OneServlet.java
@@ -0,0 +1,34 @@
+package org.example.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/one")
+public class OneServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=utf-8");
+        HttpSession session = req.getSession();
+        //获取session的ID，不同浏览器访问同一个项目的同一个页面，session的ID是不同的。
+        String id = session.getId();
+        session.setMaxInactiveInterval(7 * 86400);
+
+        PrintWriter writer = resp.getWriter();
+        //获取最大存活时间
+        int maxInactiveInterval = session.getMaxInactiveInterval();
+        writer.write("<div>OneSessionID" + id + "</div>");
+        writer.write("<div>" + maxInactiveInterval + "</div>");
+
+        req.setAttribute("name","request-name");
+        session.setAttribute("key", "session-key");
+        String reqname = (String) req.getAttribute("name");
+        writer.print(reqname + "<br>");
+        writer.print(session.getAttribute("key"));
+    }
+}
---
+++ src/main/java/org/example/controller/TwoServlet.java
@@ -0,0 +1,32 @@
+package org.example.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/two")
+
+public class TwoServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=utf-8");
+        HttpSession session = req.getSession();
+        //获取session的ID，不同浏览器访问同一个项目的同一个页面，session的ID是不同的。
+        //同意浏览器访问同一个项目的不同页面，sessionID相同。
+        String id = session.getId();
+        PrintWriter writer = resp.getWriter();
+        //获取最大存活时间
+        int maxInactiveInterval = session.getMaxInactiveInterval();
+        writer.write("<div>TwoSessionID" + id + "</div>");
+        writer.write("<div>" + maxInactiveInterval + "</div>");
+
+        String reqname = (String) req.getAttribute("name");
+        writer.print(reqname + "<br>");
+        writer.print(session.getAttribute("key"));
+    }
+}

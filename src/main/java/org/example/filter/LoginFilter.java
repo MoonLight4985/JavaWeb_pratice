865a6834d9730975fc183e9a55390fa4799294f5
moonlight <szy601870112@gmail.com>
2022-12-31 12:39:32 +0800

    登录过滤器

---
+++ src/main/java/org/example/filter/LoginFilter.java
@@ -0,0 +1,28 @@
+package org.example.filter;
+
+import org.example.entity.User;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LoginFilter implements Filter {
+    public void init(FilterConfig config) throws ServletException {
+    }
+
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        User loginUser = (User) req.getSession().getAttribute("LoginUser");
+        if (loginUser != null) {
+            chain.doFilter(request, response);
+        } else {
+            HttpServletResponse resp = (HttpServletResponse) response;
+            resp.sendRedirect(req.getContextPath() + "/index.jsp");
+        }
+    }
+}
